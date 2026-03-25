e08637d5a60fd6b45f0c9b0841f141a0be354139
Mohamed Ali <mohammed.ali@transflo.com>
2026-03-25 23:12:08 +0200

    #340532 fix build errors

--- src/Transflo.Platform.Transformer/Transflo.Platform.Transformer.Core/Models/TemplateStatus.cs
+++ src/Transflo.Platform.Transformer/Transflo.Platform.Transformer.Core/Models/TemplateStatus.cs
@@ -2,11 +2,7 @@ namespace Transflo.Platform.Transformer.Core.Models;
 
 public enum TemplateStatus
 {
-    Draft=0,// Obsolete - use version status instead
-    Active = 1,
-    Published = 2, // Obsolete - use version status instead
-    Archived = 3,
-    Deleted = 4
+    
     Draft,
     Published,
     Archived,
