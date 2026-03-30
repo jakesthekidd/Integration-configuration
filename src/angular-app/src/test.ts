a39d7b786f0dae8b094e9fe43a2799c77e273d5a
Mohamed Ali <mohammed.ali@transflo.com>
2026-03-30 16:34:22 +0200

    fix testing error

--- src/angular-app/src/test.ts
+++ src/angular-app/src/test.ts
@@ -4,4 +4,4 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestBed(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
