a347a43cf8c55d1ae9e76e484c2fc7c8288802ad
Mohamed Ali <mohammed.ali@transflo.com>
2026-03-30 21:04:39 +0200

    fix dummy test format

--- src/angular-app/src/app/dummy.spec.ts
+++ src/angular-app/src/app/dummy.spec.ts
@@ -1,5 +1,5 @@
 describe('Dummy Test', () => {
-    it('should pass to prevent 0 tests error in Karma', () => {
-        expect(true).toBe(true);
-    });
+  it('should pass to prevent 0 tests error in Karma', () => {
+    expect(true).toBe(true);
+  });
 });
